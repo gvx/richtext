a73bb82d276dbba03b46d786347f8dcbe342a67a
Robin Wellner <gyvox.public@gmail.com>
2011-01-27 11:38:30 +0100

    Only reset color mode once

--- richtext.lua
+++ richtext.lua
@@ -173,25 +173,22 @@ end
 
 local function doDraw(lines)
 	local y = 0
+	local colorMode = love.graphics.getColorMode()
 	for i, line in ipairs(lines) do -- do the actual rendering
 		y = y + line.height
 		for j, fragment in ipairs(line) do
 			if fragment.type == 'string' then
-				local colorMode = love.graphics.getColorMode()
 				love.graphics.setColorMode('modulate')
 				love.graphics.print(fragment[1], fragment.x, y - fragment.height)
 				if rich.debug then
 					love.graphics.rectangle('line', fragment.x, y - fragment.height, fragment.width, fragment.height)
 				end
-				love.graphics.setColorMode(colorMode)
 			elseif fragment.type == 'img' then
-				local colorMode = love.graphics.getColorMode()
 				love.graphics.setColorMode('replace')
 				love.graphics.draw(fragment[1][1], fragment.x, y - fragment[1].height)
 				if rich.debug then
 					love.graphics.rectangle('line', fragment.x, y - fragment[1].height, fragment[1].width, fragment[1].height)
 				end
-				love.graphics.setColorMode(colorMode)
 			elseif fragment.type == 'font' then
 				love.graphics.setFont(fragment[1])
 			elseif fragment.type == 'color' then
@@ -199,6 +196,7 @@ local function doDraw(lines)
 			end
 		end
 	end
+	love.graphics.setColorMode(colorMode)
 	return y
 end
 
